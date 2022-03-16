bac0c304b47688dd9ec21c62be10c4412f46c92b
Nemanja <nemanja94email@gmail.com>
2022-03-16 11:25:30 +0100

    add models - user and place

---
+++ models/Place.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose"
+
+const placeSchema = new mongoose.Schema({
+  location: {
+    type: String, 
+    required: true, 
+    minlength: [1, "Must be at least 1 character"],
+    maxlength: [100, "Must be less that 100 characters"]
+  },
+  title: {
+    type: String,
+    required: true,
+    minlength: [1, "Must be at least 1 character"],
+    maxlength: [100, "Must be less that 100 characters"]
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: [1, "Must be at least 1 character"],
+  },
+  star: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  long: {
+    type: Number,
+    required: true
+  },
+  lat: {
+    type: Number,
+    required: true
+  },
+  available: {
+    type: Boolean,
+    default: true
+  }
+  /* Make relationship between place and a user that created it */
+});
+
+module.exports = mongoose.model("Place", placeSchema);
---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please enter your first name"],
+    minlenght: [2, "Must be at least 2 characters long"],
+    maxlenght: [20, "Must be less than 20 characters long"]
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please enter your last name"],
+    minlenght: [2, "Must be at least 2 characters long"],
+    maxlenght: [20, "Must be less than 20 characters long"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email address"],
+    unique: [true, "User with this email already exists"],
+  },
+  phoneNumber: {
+    type: String,
+    validate: {
+      validator: function(num) {
+        return /\d{3}-\d{3}-\d{3}/.test(num)
+      },
+      message: props => `${props.value} is not a valid phone number!` 
+    }
+  },
+  password: {
+    type: String,
+  },
+  /* Make relationship between user and posts he created */
+})
